755c62e40a1d6b2460d08b5dddee84fe5c6482f4
Samitti <samuelteweldebrhan@gmail.com>
2020-06-03 17:50:58 +0300

    create comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_135821) do
+ActiveRecord::Schema.define(version: 2020_06_03_142521) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
